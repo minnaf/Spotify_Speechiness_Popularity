0eff09b65a66f16020089e1da654a5ac454fa521
Minna Fingerhood <mfingerhood@gmail.com>
2019-07-12 13:08:54 -0400

    day 2 revisions afternoon

--- README.md
+++ README.md
@@ -14,16 +14,16 @@ Goals of this project:
 
 Outline of Project:
 
-1. Gather playlist data from Spotify API 
-2. Gather song data from Spotify API using song_ids in the playlist
-3. Create SQL database and tables of the data
-4. Organize the data 
+1. Gather playlist data from Spotify API and put in SQL database
+2. Gather song data from Spotify API using song_ids in the playlist and put into SQL table
+3. Create Pandas DataFrame 
+4.  
 5. 
 -------------------
 
 
 1. To gather playlist data from the Spotify API, we used requests.get(). Spotify's API documentation requies an OAuth Token and playlist_ID, which gathered from spotify's developer website and app. 
-We retrieved this data and subsequently organized it by isolating categories of (1) song name, (2) song_id, (3) popularity and (4) artist_name. We then converted this information into a pandas DF for easier comprehension and accessibility. 
+We retrieved this data and subsequently organized it by isolating categories of (1) song name, (2) song_id, (3) popularity and (4) artist_name into a SQL database. 
 
-2. Using the column, 'song_id,' we retrieved audio features of the tracks which included information such as:  "danceability," "energy," "key," "loudness," "mode," "speechiness," "acousticness," "instrumentalness," "liveness," "valence," and "tempo." 
+2. Using the column in our SQL table, 'song_id,' we retrieved audio features of the tracks which included information such as:  "danceability," "energy," "key," "loudness," "mode," "speechiness," "acousticness," "instrumentalness," "liveness," "valence," and "tempo." We extracted danceability and speechiness and inputted it into our SQL table. Then, we converted to pandas dataframe for greate accessibility. 
 
--- minna_functions.py
+++ minna_functions.py
@@ -4,34 +4,42 @@ import pandas as pd
 import sqlite3
 
 conn = sqlite3.connect('spotify.db')
+c = conn.cursor()
+
 
 class get_spotify_playlist():
+    
+    def __init__(self, OAuth_token):
+        self.headers_value = OAuth_token
+        self.headers = {'Authorization':f'Bearer {self.headers_value}'}
         
-    def get_playlist_SQL(self, playlist_id, OAuth_token, table_name):
-        '''takes in playlist_id and OAuth token from spotify API, pulls from API, and returns SQL database '''
-        headers_value = OAuth_token
-        headers = {'Authorization':f'Bearer {headers_value}'}
-        resp = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers = headers)
+    def get_playlist_SQL(self, playlist_id, table_name):
+        '''takes in playlist_id and pulls from API, and returns SQL database '''
+        #headers_value = OAuth_token
+        #headers = 
+        resp = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers = self.headers)
+        data_json = resp.json()
         
-        #name_id_df = pd.DataFrame(columns = ['song_name', 'song_id'])
         
         c = conn.cursor()
         c.execute(f'''CREATE TABLE IF NOT EXISTS {table_name}
-                 ([generated_id] INTEGER PRIMARY KEY,
+                 ([id] INTEGER PRIMARY KEY,
                  [song_name] text, 
                  [song_id] text,
                  [popularity] integer,
-                 [artist_name] text)''')
+                 [artist_name] text,
+                 [speechiness] float, 
+                 [danceability] float)''')
 
         
-        for i in range(len(resp.json()['tracks']['items'])):  
+        for i in data_json['tracks']['items']:  
             c.execute(f'''INSERT INTO {table_name} 
-                    ([song_name], song_id, popularity, artist_name)
+                    (song_name, song_id, popularity, artist_name)
                     VALUES(?,?,?,?)''', 
-                    (resp.json()['tracks']['items'][i]['track']['name'],
-                     resp.json()['tracks']['items'][i]['track']['id'],
-                     resp.json()['tracks']['items'][i]['track']['popularity'],
-                     resp.json()['tracks']['items'][i]['track']['artists'][0]['name']))
+                    (i['track']['name'],
+                     i['track']['id'],
+                     i['track']['popularity'],
+                     i['track']['artists'][0]['name']))
     
             conn.commit()
          
@@ -39,21 +47,43 @@ class get_spotify_playlist():
         
         
         c.fetchall()
-#         c.close()
-#         conn.close()
+
                 
-        
-    def SQL_to_dataframe(self, table_name):
+    @staticmethod
+    def SQL_to_dataframe(table_name):
         '''takes in SQL table_name and converts to pandas dataframe, returning the head of the dataframe'''
-        df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
+        df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn, index_col='id')
         return df    
    
 
- class get_spotify_songs(): 
+    def song_audio_features(self, df, table_name):
+        '''takes in dataframe table and corresponding SQL table and returns dataframe with audio features for the songs'''
+       
+        song_ids = list(df['song_id'])
         
-        def __init__(self, song_ids):
-            self.song_ids = 
-    
+        
+        if len(song_ids) < 100:
+            formatted = (",".join(song_ids))
+            resp = requests.get(f'https://api.spotify.com/v1/audio-features?ids={formatted}', headers = self.headers)
+            data_json = resp.json()
+            
+            for i in data_json['audio_features']:
+                c.execute(f'''UPDATE {table_name} 
+                              SET speechiness = ?, 
+                                  danceability = ?
+                              WHERE song_id = ?''', (i['speechiness'], i['danceability'], i['id']))
+                conn.commit()
+         
+            return get_spotify_playlist.SQL_to_dataframe(f'{table_name}')
+        
+        
+        else:
+            return 'length of song_ids is > 100, has to be divided' 
+            
+            
+class statistics(): 
+    pass
+        
     
     
     
